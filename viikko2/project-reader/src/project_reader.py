2da5a8c99b740c41059f5f34047a87defc844b59
Tomi Nieminen <tomi.s.nieminen@helsinki.fi>
2024-11-05 13:40:52 +0200

    Completed Tehtava 2

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,6 +1,6 @@
 from urllib import request
 from project import Project
-
+import toml
 
 class ProjectReader:
     def __init__(self, url):
@@ -9,7 +9,22 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        loaded = toml.loads(content)
+        name = loaded["tool"]["poetry"]["name"]
+        description = loaded["tool"]["poetry"]["description"]
+        license = loaded["tool"]["poetry"]["license"]
+        authors = loaded["tool"]["poetry"]["authors"]
+        dependencies = loaded["tool"]["poetry"]["dependencies"]
+        group_dependencies = loaded["tool"]["poetry"]["group"]["dev"]["dependencies"]
+        author_list = []
+        for one in authors: 
+            author_list.append(f"\n- {one}")
+        dependency_list = []
+        for one in dependencies: 
+            dependency_list.append(f"\n- {one}")
+        group_dependency_list = []
+        for one in group_dependencies: 
+            group_dependency_list.append(f"\n- {one}")
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(name, description, license, author_list, dependency_list, group_dependency_list)
