115bf36a3bd159b7b1f2818b3b4ed4f652bf312e
Tomi <tomi.nieminen@tutanota.com>
2024-11-11 16:30:35 +0200

    Graphic nhl-reader

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,39 @@
-from player import Player
+from rich.console import Console 
+from rich.table import Table
+from player import PlayerReader, PlayerStats 
+
+console =  Console()
+
+def create_table(nationality, season): 
+    table = Table(title=f"Top scorers of {nationality} season {season}")
+    table.add_column("name", justify="left", style="cyan")
+    table.add_column("team", justify="left", style="purple")
+    table.add_column("goals", justify="right", style="green")
+    table.add_column("assists", justify="right", style="green")
+    table.add_column("points", justify="right", style="green")
+    return table 
 
 def main():
-    pass
+    print()
+    print("NHL statistics by nationality")
+    print()
+    season = console.input("Select season [purple][2018-19/2019-20/2020-21/2021-22/2022-23/2023-24/][/]: ")
+    url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
+    reader = PlayerReader(url)
+    print()
+    print("Select nationality")
+    nationality = console.input("[purple][AUT/CZE/AUS/SWE/GER/DEN/SUI/SVK/NOR/RUS/CAN/LAT/BLR/SLO/USA/FIN/GBR)][/]: ")
+    print()
+    while nationality != "":
+        stats = PlayerStats(reader)
+        players = stats.top_scorers_by_nationality(nationality)
+        table = create_table(nationality, season) 
+        for player in players:
+            table.add_row(str(player.name), str(player.team), str(player.goals), str(player.assists), str(player.points))
+        console.print(table)
+        print()
+        print("Select nationality")
+        nationality = console.input("[purple][AUT/CZE/AUS/SWE/GER/DEN/SUI/SVK/NOR/RUS/CAN/LAT/BLR/SLO/USA/FIN/GBR)][/]: ")
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,33 @@
+import requests
+
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.team = dict['team']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
+        self.points = self.goals + self.assists
+        self.nationality = dict['nationality']
     
     def __str__(self):
-        return self.name
+        return "{:<20} {:3}  {:2} + {:2} = {:<3}".format(self.name, self.team, self.goals, self.assists, self.goals+self.assists)
+
+class PlayerReader: 
+    def __init__(self, url):
+        self.players = []
+        response = requests.get(url).json()
+        for player_dict in response:
+            player = Player(player_dict)
+            self.players.append(player)
+
+class PlayerStats: 
+    def __init__(self, reader):
+        self.reader = reader
+
+    def top_scorers_by_nationality(self, nation): 
+        stats = []
+        for player in self.reader.players:
+            if player.nationality == nation: 
+                stats.append(player) 
+        stats.sort(key=lambda player: player.points, reverse=True) 
+        return stats
