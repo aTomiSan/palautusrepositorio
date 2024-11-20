20944a3690870564d6dc8ea909e1803fd7c1d1d5
Tomi <tomi.nieminen@tutanota.com>
2024-11-20 12:53:49 +0200

    bugikorjaus

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -8,8 +8,8 @@ logger("aloitetaan ohjelma!")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-print(f"Lukujen {x} ja {y} summa on {summa(x, y)}")
-print(f"Lukujen {x} ja {y} erotus on {erotus(x, y)}")≈
+print(f"Lukujem {x} ja {y} summa on {summa(x, y)}")
+print(f"Lukujem {x} ja {y} erotus on {erotus(x, y)}")
 
 logger("lopetetaan")
 print("goodbye!") 
